68f84260d860b00f6f9915f18c26f4e10132bcd0
NirjalaP <nirjala.prajapati11@gmail.com>
2025-04-13 09:19:37 -0700

    Implemented local authentication

--- index.js
+++ index.js
@@ -38,3 +38,9 @@ constUserDetail = new Schema({
 
 UserDetail.plugin(passportLocalMongoose);
 const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+// Passport local authentication
+passport.use(UserDetails.createStrategy());
+
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
