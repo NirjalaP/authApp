ef9a894838714d4cca70a5e8efde1959b3f6fbc9
NirjalaP <nirjala.prajapati11@gmail.com>
2025-04-13 09:02:17 -0700

    Added index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(_dirname));
+
+const bodyParser = require("body-parse");
+const expressSession = require('express-session')({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSession);
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log('App Listening on port ' + port));
